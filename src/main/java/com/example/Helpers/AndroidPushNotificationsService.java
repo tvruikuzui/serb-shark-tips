61fda469f54020a462038c620d50c384c069cd5a
Aharon Luzon <ahron901@gmail.com>
2017-04-30 12:13:34 +0300

    main serv

---
+++ src/main/java/com/example/Helpers/AndroidPushNotificationsService.java
@@ -0,0 +1,34 @@
+package com.example.Helpers;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Created by User on 30/04/2017.
+ */
+
+@Service
+public class AndroidPushNotificationsService {
+    private static final String FIREBASE_SERVER_KEY = "xxxxxxxxxxxxxxxxxxxxxxxHqgVED6gWSwI_G4z7hnA";
+
+    @Async
+    public CompletableFuture<FirebaseResponse> send(HttpEntity<String> entity) {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ArrayList<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+        interceptors.add(new HeaderRequestInterceptor("Authorization", "key=" + FIREBASE_SERVER_KEY));
+        interceptors.add(new HeaderRequestInterceptor("Content-Type", "application/json"));
+        restTemplate.setInterceptors(interceptors);
+
+        FirebaseResponse firebaseResponse = restTemplate.postForObject("https://fcm.googleapis.com/fcm/send", entity, FirebaseResponse.class);
+
+        return CompletableFuture.completedFuture(firebaseResponse);
+    }
+}
